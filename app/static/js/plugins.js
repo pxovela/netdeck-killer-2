3159dd865d1416d3cf6b8cd8c918c2b1da336f49
pxovela <irakli.py@gmail.com>
2020-08-13 14:13:12 +0800

    added home page

--- app/static/js/plugins.js
+++ app/static/js/plugins.js
@@ -15,8 +15,8 @@ if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
 $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
-    if ($(window).scrollTop() < 300) { //if window scrolled 300px or less from top
-    $(".navbar").toggleClass("solid-toggle"); //add the solid class to navbar
+    if ($(window).scrollTop() <= 300) { //if window scrolled 300px or less from top
+    $("nav.navbar").toggleClass("solid-toggle"); //add the solid class to navbar
     }
 });
 
@@ -36,61 +36,128 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 })
 
 /*========== BOUNCING DOWN ARROW ==========*/
-
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
+    });
+});
 
 /*========== LIGHTBOX IMAGE GALLERY ==========*/
-
+$(document).ready(function () {
+    lightbox.option({
+        'resizeDuration': 600,
+        'wrapAround': false,
+        'imageFadeDuration': 500
+    });
+});
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-
+$(document).ready(function(){ //when document(DOM) loads completely
+    $('#team-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: false, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //768px width and up display 2 items
+            },
+			992 : {
+                items: 3 //992px width and up display 2 items
+            }
+        }
+    });
+  });
 
 /*========== SKILLS COUNTER ==========*/
-
+$(document).ready(function () {
+    $(".counter").counterUp({
+        delay: 10,
+        time: 2000,
+    })
+})
 
 /*========== CLIENTS CAROUSEL ==========*/
-
+$(document).ready(function(){ //when document(DOM) loads completely
+    $('#clients-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: false, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //768px width and up display 2 items
+            },
+			992 : {
+                items: 3 //992px width and up display 2 items
+            }
+        }
+    });
+  });
 
 /*========== TOP SCROLL BUTTON ==========*/
-
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.top-scroll').fadeIn();
+        } else {
+            $('.top-scroll').fadeOut();
+        }
+    });
+});
 
 /*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE 
-
 **Remember to use "animate__animated animate__fadeInUp" ==========*/
-
+$(document).ready(function () {
+    if ($(window).width() < 768) {
+        $('div').attr('data-animation', 'animate__animated animate__fadeInUp');
+        $('div').attr('date-delay', '0s')
+    }
+});
 
 
 /*========== WAYPOINTS ANIMATION DELAY ==========*/
 //Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
 $(function () { // a self calling function
-  function onScrollInit(items, trigger) { // a custom made function
-      items.each(function () { //for every element in items run function
-          var osElement = $(this), //set osElement to the current
-              osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
-              osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
-
-          osElement.css({ //change css of element
-              '-webkit-animation-delay': osAnimationDelay, //for safari browsers
-              '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
-              'animation-delay': osAnimationDelay //normal
-          });
-
-          var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
-
-          osTrigger.waypoint(function () { //scroll upwards and downwards
-              osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
-          }, {
-                  triggerOnce: true, //only once this animation should happen
-                  offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
-              });
-      });
-  }
-
-  onScrollInit($('.os-animation')); //function call with only items
-  onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
-});
-
-
-/*========== CONTACT FORM INPUT VALIDATION ==========*/
+    function onScrollInit(items, trigger) { // a custom made function
+        items.each(function () { //for every element in items run function
+            var osElement = $(this), //set osElement to the current
+                osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
+                osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
+  
+            osElement.css({ //change css of element
+                '-webkit-animation-delay': osAnimationDelay, //for safari browsers
+                '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
+                'animation-delay': osAnimationDelay //normal
+            });
+  
+            var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+  
+            osTrigger.waypoint(function () { //scroll upwards and downwards
+                osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
+            }, {
+                    triggerOnce: true, //only once this animation should happen
+                    offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
+                });
+        });
+    }
+  
+    onScrollInit($('.os-animation')); //function call with only items
+    onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+  });
+  
+  
+  /*========== CONTACT FORM INPUT VALIDATION ==========*/
 //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
 $(function () {
 
@@ -106,7 +173,7 @@ $(function () {
 
       // if the validator does not prevent form submit
       if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php";
+          var url = "contact/gmail-contact.php";
 
           // POST values in the background the the script URL
           $.ajax({
