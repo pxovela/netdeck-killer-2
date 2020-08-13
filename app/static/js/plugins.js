b09468fea0d6d40574c82dc981bda47649f2f299
pxovela <irakli.py@gmail.com>
2020-08-13 17:45:40 +0800

    added select regions section

--- app/static/js/plugins.js
+++ app/static/js/plugins.js
@@ -116,6 +116,33 @@ $(document).ready(function () {
     });
 });
 
+/*========== region Select Checkbox ==========*/
+// image gallery
+// init the state from the input
+$(".image-checkbox").each(function () {
+    if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
+      $(this).addClass('image-checkbox-checked');
+    }
+    else {
+      $(this).removeClass('image-checkbox-checked');
+    }
+  });
+  
+  // sync the state to the input
+  $(".image-checkbox").on("click", function (e) {
+    $(this).toggleClass('image-checkbox-checked');
+    var $checkbox = $(this).find('input[type="checkbox"]');
+    $checkbox.prop("checked",!$checkbox.prop("checked"))
+  
+    e.preventDefault();
+  });
+
+  var limit = 3;
+  $('input.image-checkbox').on('change', function(evt) {
+     if($(this).siblings('image-checkbox-checked').length >= limit) {
+         this.checked = false;
+     }
+  });
 /*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE 
 **Remember to use "animate__animated animate__fadeInUp" ==========*/
 $(document).ready(function () {
--- app/views.py
+++ app/views.py
@@ -13,7 +13,29 @@ import os
 # library to secure filenames
 from werkzeug.utils import secure_filename
 
+#list of all champs and regions
+regions = ['Bilgewater', 'Demacia', 'Freljord', 'Ionia', 'Noxus', 'Piltover & Zaun', 'Shadow Isles']
+
 # create a main view
 @app.route('/')
 def index():
    return render_template("public/killer-index.html")
+
+# route for region select
+@app.route('/region_select')
+def region_select():
+   return render_template("public/region-select.html", regions=regions)
+
+# route for region select
+@app.route('/champion_select', methods=['GET', 'POST'])
+def champion_select():
+   if request.method == 'POST':
+      if len(request.form.getlist('region_check'))==0:
+         region_error = "Select at least 1 region!"
+         return render_template("public/region-select.html", regions=regions, region_error=region_error)
+      elif len(request.form.getlist('region_check')) > 2:
+         region_error = "You can select no more than TWO regions!"
+         return render_template("public/region-select.html", regions=regions, region_error=region_error)
+      else:
+         render_template("public/champion-select.html", regions=regions)
+   return render_template("public/champion-select.html", regions=regions)
